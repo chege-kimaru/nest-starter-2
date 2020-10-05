b7e5f494ffccee0c035a17433827738b0cb22b98
chege-kimaru <chege-kimaru@gmail.com>
2020-10-05 22:22:56 +0300

    fix: fix issues causing the app to crash on startup

--- src/sequelize/migrations/20200528132151-create-user-role.js
+++ src/sequelize/migrations/20200528132151-create-user-role.js
@@ -1,51 +1,54 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('UserRoles', {
-      id: {
-        allowNull: false,
-        primaryKey: true,
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4
+    return queryInterface.createTable(
+      'UserRoles',
+      {
+        id: {
+          allowNull: false,
+          primaryKey: true,
+          type: Sequelize.UUID,
+          defaultValue: Sequelize.UUIDV4,
+        },
+        userId: {
+          type: Sequelize.UUID,
+          allowNull: false,
+          onDelete: 'RESTRICT',
+          onUpdate: 'RESTRICT',
+          references: {
+            model: 'Users',
+            key: 'id',
+          },
+        },
+        roleId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          onDelete: 'RESTRICT',
+          onUpdate: 'RESTRICT',
+          references: {
+            model: 'Roles',
+            key: 'id',
+          },
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
       },
-      userId: {
-        type: Sequelize.UUID,
-        allowNull: false,
-        onDelete: 'RESTRICT',
-        onUpdate: 'RESTRICT',
-        references: {
-          model: 'Users',
-          key: 'id'
-        }
+      {
+        uniqueKeys: {
+          uniqueUserIdRoleId: {
+            fields: ['userId', 'roleId'],
+          },
+        },
       },
-      roleId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        onDelete: 'RESTRICT',
-        onUpdate: 'RESTRICT',
-        references: {
-          model: 'Roles',
-          key: 'id'
-        }
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    }, {
-      indexes: [
-        {
-          unique: true,
-          fields: ['userId', 'roleId']
-        }
-      ]
-    });
+    );
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.dropTable('UserRoles');
-  }
+  },
 };
